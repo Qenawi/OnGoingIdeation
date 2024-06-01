404dac9506b66816d225d64b46be689834723616
qenawi <aldmanmero@yahoo.com>
2024-06-01 07:12:41 +0300

    - adding track pad

--- ui/src/main/java/com/example/ui/telda/DragDropScreen.kt
+++ ui/src/main/java/com/example/ui/telda/DragDropScreen.kt
@@ -1,40 +1,70 @@
 package com.example.ui.telda
 
-import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
-import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+sealed class ItemType {
+    class Icon(val vectorDrawable: ImageVector) : ItemType()
+    class Text : ItemType()
+
+}
 
 @Preview(showBackground = true, name = "CardEditScreen", showSystemUi = false)
 @Composable
 fun CardEditScreen() {
     val ctx = LocalContext.current
-    var cardColor = remember { mutableStateOf(Color.LightGray) }
-    MaterialTheme(colorScheme = MaterialTheme.colorScheme.copy(primary = Color.Black))
-    {
+    val cardColor = remember { mutableStateOf(Color.LightGray) }
+    val children = remember { mutableStateOf(listOf<ItemType>()) }
+    MaterialTheme(colorScheme = MaterialTheme.colorScheme.copy(primary = Color.Black)) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -46,13 +76,26 @@ fun CardEditScreen() {
                     .weight(1f)
             ) {
                 Text(text = "Customize Card Screen", modifier = Modifier.padding(start = 12.dp))
-                DropArea(modifier = Modifier.padding(start = 8.dp, top = 20.dp, bottom = 20.dp), cardColor = cardColor.value)
-                Text(text = "Customize Card Screen", modifier = Modifier.padding(start = 12.dp))
-                ColorSelection(
-                    modifier = Modifier.padding(start = 12.dp, top = 12.dp),
-                    onColorSelected = {
-                        cardColor.value = it
-                    })
+                DropArea(
+                    modifier = Modifier.padding(start = 8.dp, top = 20.dp, bottom = 20.dp),
+                    cardColor = cardColor.value,
+                    children = children.value
+                )
+                Text(
+                    text = "Customize Card Screen",
+                    modifier = Modifier.padding(start = 12.dp, bottom = 12.dp, top = 12.dp)
+                )
+                ColorSelection(modifier = Modifier.padding(start = 12.dp), onColorSelected = {
+                    cardColor.value = it
+                })
+                Text(
+                    text = "Add Element",
+                    modifier = Modifier.padding(start = 12.dp, bottom = 12.dp, top = 24.dp)
+                )
+                AddElement(modifier = Modifier.padding(start = 12.dp), addItemType = {
+                    children.value += it
+                })
+
             }
             /**
              * Save And Reset Area
@@ -60,12 +103,14 @@ fun CardEditScreen() {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(20.dp)
-            )
-            {
+            ) {
+
                 Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(12.dp)) {
                     Text(text = "Save")
                 }
-                Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(12.dp)) {
+                Button(
+                    onClick = { children.value = emptyList() }, shape = RoundedCornerShape(12.dp)
+                ) {
                     Text(text = "Reset")
                 }
             }
@@ -76,20 +121,106 @@ fun CardEditScreen() {
 
 }
 
+/**
+ * [AddElement]
+ */
+@Composable
+fun AddElement(modifier: Modifier = Modifier, addItemType: (ItemType) -> Unit) {
+    val expanded = remember { MutableTransitionState(false) }
+    Column(modifier = modifier) {
+        OutlinedButton(
+            onClick = {
+                addItemType.invoke(ItemType.Text())
+
+            }, shape = RoundedCornerShape(12.dp)
+        ) {
+            Text(text = "Add Text")
+        }
+
+        OutlinedButton(
+            onClick = {
+                expanded.targetState = true
+            }, shape = RoundedCornerShape(12.dp)
+        ) {
+            Text(text = "Add Icon")
+        }
+        AnimatedVisibility(visibleState = expanded) {
+            Row {
+                for (i in listOf(Icons.Default.Add, Icons.Default.Refresh, Icons.Default.Call)) {
+                    IconButton(onClick = {
+                        expanded.targetState = false
+                        addItemType.invoke(ItemType.Icon(i))
+                    }) {
+                        Icon(imageVector = i, contentDescription = "")
+                    }
+
+                }
+            }
+        }
+    }
+
+}
+
+/**
+ * [ColorSelection]
+ */
 @Composable
 fun ColorSelection(modifier: Modifier = Modifier, onColorSelected: (Color) -> Unit) {
-    Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(10.dp))
-    {
-        for (i in listOf(Color.LightGray,Color.Red, Color.Green, Color.Blue, Color.Yellow)) {
-            Box(
-                modifier = Modifier
-                    .size(30.dp)
-                    .shadow(2.dp, shape = CircleShape)
-                    .background(i, CircleShape)
-                    .clickable {
-                        onColorSelected(i)
-                    })
+    Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+        for (i in listOf(Color.LightGray, Color.Red, Color.Green, Color.Blue, Color.Yellow)) {
+            Box(modifier = Modifier
+                .size(30.dp)
+                .shadow(2.dp, shape = CircleShape)
+                .background(i, CircleShape)
+                .clickable {
+                    onColorSelected(i)
+                })
         }
     }
 }
 
+
+@Preview
+@Composable
+fun TrackPad() {
+    var scale by remember { mutableFloatStateOf(1f) }
+    var offsetX by remember { mutableFloatStateOf(0f) }
+    var offsetY by remember { mutableFloatStateOf(0f) }
+    val state = rememberTransformableState { zoomChange, panChange, rotationChange ->
+        scale = (scale * zoomChange).coerceIn(0.2f, 3f)
+        offsetX = (offsetX + panChange.x)
+        offsetY = (offsetY + panChange.y)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "TrackPad",
+            color = Color.Black,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(bottom = 12.dp)
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(
+                    Color.LightGray, RoundedCornerShape(12.dp)
+                )
+                .transformable(state)
+        ) {
+            Surface(modifier = Modifier
+                .size(120.dp, 60.dp)
+                .scale(scale)
+                .offset { IntOffset(offsetX.toInt(), offsetY.toInt()) },
+                shape = RoundedCornerShape(12.dp),
+                content = {})
+        }
+
+    }
+}
--- ui/src/main/java/com/example/ui/telda/trackableDropAreaParent.kt
+++ ui/src/main/java/com/example/ui/telda/trackableDropAreaParent.kt
@@ -1,42 +1,66 @@
 package com.example.ui.telda
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.DraggableState
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.anchoredDraggable
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.roundToInt
 
 
 @Preview(showBackground = true, name = "DropArea")
 @Composable
-fun DropArea(modifier: Modifier = Modifier, cardColor:Color = Color.LightGray)
-
-   {
-
-    val items = listOf("Item 1", "Item 2", "Item 3")
-    val cardColorStatus by animateColorAsState(cardColor, animationSpec = tween(1000) ,label = "Card Color")
+fun DropArea(
+    modifier: Modifier = Modifier,
+    cardColor: Color = Color.LightGray,
+    children: List<ItemType> = emptyList()
+) {
+    val colorChangeAnimationSpec = tween<Color>(1000, easing = FastOutSlowInEasing)
+    val cardColorStatus by animateColorAsState(
+        cardColor,
+        animationSpec = colorChangeAnimationSpec,
+        label = "Card Color"
+    )
     Column(
         modifier =
         modifier
@@ -65,13 +89,44 @@ fun DropArea(modifier: Modifier = Modifier, cardColor:Color = Color.LightGray)
                     Text(text = "titanium", fontSize = 12.sp)
 
                 }
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)) {}
+                /**
+                 * Drag drop Area
+                 */
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) {
+
+                    /*
+                    Editable Area
+                     */
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(1.dp, Color.Black)
+                            .padding(8.dp)
+                    ) {
+                        children.forEach {
+                            DraggableItem(it)
+                        }
+                    }
+                    /*
+                     un Editable Area
+                     */
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(50.dp)
+                    )
+                    {
+
+                    }
+                }
                 /*
                 Bottom Area
                  */
-
                 Row {
                     Spacer(modifier = Modifier.weight(1f))
                     Text(text = "MasterCard logo", fontSize = 12.sp)
@@ -81,4 +136,41 @@ fun DropArea(modifier: Modifier = Modifier, cardColor:Color = Color.LightGray)
         }
 
     }
+}
+
+
+@Composable
+fun DraggableItem(type: ItemType) {
+    var scale by remember { mutableFloatStateOf(1f) }
+    var offsetX by remember { mutableFloatStateOf(0f) }
+    var offsetY by remember { mutableFloatStateOf(0f) }
+    val state = rememberTransformableState { zoomChange, panChange, rotationChange ->
+         scale = (scale * zoomChange)
+         offsetX = (offsetX + panChange.x)
+         offsetY = (offsetY+panChange.y)
+    }
+    Box(modifier = Modifier.size(120.dp)
+        .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+        .border(2.dp,Color.Black)
+        .transformable(state).scale(scale)
+    )
+    {
+        when (type) {
+            is ItemType.Icon -> {
+                Icon(
+                    imageVector = type.vectorDrawable,
+                    contentDescription = "",
+                )
+            }
+
+            is ItemType.Text -> {
+                Text(text = "${scale.roundToInt()}")
+            }
+        }
+
+    }
+
+
+
+
 }
